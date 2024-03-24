d22495c0f046b95b8fa8f0f29046aacb8193f01b
Anatolii Tsyplenkov <atsyplenkov@gmail.com>
2024-03-24 21:51:57 +1300

    R CMD Check

--- R/fil_wt_sf.R
+++ R/fil_wt_sf.R
@@ -1,7 +1,11 @@
+#' @noRd
+#' @export
 fil_wt_sf <-
   function(
     input,
-    ...
+    lamda = 10,
+    order = 2,
+    keep_start = TRUE
   ){
 
     crs <- sf::st_crs(input)
@@ -26,28 +30,40 @@ fil_wt_sf <-
         )
     }
 
+    # Perform smoothing
     x_filt <- fil_wt_x(
       y = coords_x,
       x = distances,
-      ...
+      lambda = lamda,
+      order = order
     )
 
     y_filt <- fil_wt_x(
       y = coords_y,
       x = distances,
-      ...
+      lambda = lamda,
+      order = order
     )
 
-    # Keep start and end points
-    x_filt[1] <- coords_x[1]
-    x_filt[n] <- coords_x[n]
+    if (keep_start) {
+      # Keep start and end points
+      x_filt[1] <- coords_x[1]
+      x_filt[n] <- coords_x[n]
 
-    y_filt[1] <- coords_y[1]
-    y_filt[n] <- coords_y[n]
+      y_filt[1] <- coords_y[1]
+      y_filt[n] <- coords_y[n]
 
-    # Create LINESTRING and return sf object
-    to_linestring(x_filt, y_filt) |>
-      sf::st_as_sfc(crs = crs) |>
-      sf::st_as_sf()
-
+      # Create LINESTRING and return sf object
+      smoothed_line <-
+        to_linestring(x_filt, y_filt) |>
+        sf::st_as_sfc(crs = crs) |>
+        sf::st_as_sf()
+      return(smoothed_line)
+    } else {
+      smoothed_line <-
+        to_linestring(x_filt, y_filt) |>
+        sf::st_as_sfc(crs = crs) |>
+        sf::st_as_sf()
+      return(smoothed_line)
+    }
   }
